1583996f412a1044f71dbedebf338ea2679ade4f
Antonio Ávila <81250968+koavdev@users.noreply.github.com>
2024-06-29 11:21:40 -0300

    feat: get newest jobs data

--- djob_frontend/pages/index.vue
+++ djob_frontend/pages/index.vue
@@ -1,3 +1,9 @@
+<script setup>
+
+    const { data: jobs } = await useFetch('http://localhost:8000/api/v1/jobs/newest/')
+    console.log(jobs.value)
+</script>
+
 <template>
     <div>
         <div class="py-20 px-6 bg-teal-700 text-center">
@@ -12,9 +18,10 @@
 
             <div class="space-y-4">
 
-                <Job />
-                <Job />
-                <Job />
+                <Job
+                    v-for="job in jobs"
+                    v-bind:key="job.id"
+                    v-bind:job="job"/>
                 
             </div>
 
