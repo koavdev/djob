08f64197c66e85d72a8f7737413c7952b34021da
Antonio Ávila <81250968+koavdev@users.noreply.github.com>
2024-06-29 19:11:29 -0300

    feat: login on mounted

--- djob_frontend/app.vue
+++ djob_frontend/app.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '~/stores/user'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.initStore()
+})
+
+</script>
 <template>
   <div>
     <NuxtLayout>
