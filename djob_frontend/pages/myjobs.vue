ad86df693f1ab9cdc77ca4233dc32d9151470be0
Antonio Ávila <81250968+koavdev@users.noreply.github.com>
2024-06-29 19:23:22 -0300

    feat: MyJobs request

--- djob_frontend/pages/myjobs.vue
+++ djob_frontend/pages/myjobs.vue
@@ -1,9 +1,45 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '~/stores/user'
+
+const userStore = useUserStore()
+const router = useRouter()
+let jobs = ref()
+
+onMounted(() => {
+  if (!userStore.user.isAuthenticated) {
+    router.push('/login')
+  } else {
+    getJobs()
+  }
+})
+
+async function getJobs() {
+    $fetch('http://localhost:8000/api/v1/jobs/my', {
+        headers: {
+            'Authorization': 'token ' + userStore.user.token,
+            'Content-Type': 'application/json'
+        },
+    }).then(response => {
+        jobs.value = response
+    }).catch( error => {
+        console.log('error', error)
+    })
+}
+
+</script>
+
 <template>
     <div class="py-10 px-6">
         <h1 class="mb-6 text-2xl">My jobs</h1>
 
         <div class="space-y-4">
-                <Job :my="true"/>
+                <Job 
+                    v-for="job in jobs"
+                    v-bind:key="job.id"
+                    :job="job"
+                    :my=true
+                    />
             </div>
     </div>
 </template>
